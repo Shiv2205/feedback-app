0fb515e243f1833e8cb690e1ac31363a71ea6673
Shivam Veerabudren <shivamveerabudren@gmail.com>
2022-03-03 13:11:35 -0500

    Changed from json server fetch to fetch from json file

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,25 +1,25 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState } from 'react' //, useEffect
 import { v4 as uuidv4 } from 'uuid' //used to generate unique id
-//import FeedbackData from '../data/FeedbackData'
+import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true)
-    const [feedback, setFeedback] = useState([])
+    const [feedback, setFeedback] = useState(FeedbackData)
 
-    useEffect(() => {
-        fetchFeedback()
-    }, [])
+    // useEffect(() => {
+    //     fetchFeedback()
+    // }, [])
 
-    //Fetch feedback from json server
-    const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
-        const data = await response.json()
+    // //Fetch feedback from json server
+    // const fetchFeedback = async () => {
+    //     const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    //     const data = await response.json()
 
-        setFeedback(data)
-        setIsLoading(false)
-    }
+    //     setFeedback(data)
+    //     setIsLoading(false)
+    // }
 
     //Delete feedback
     const deleteFeedback = (id) => {
