18b6ec389ade9c828bc544bb28dfed6e3c6b0a39
Shivam Veerabudren <shivamveerabudren@gmail.com>
2022-03-03 13:17:11 -0500

    Corrected typo

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ function AboutPage({ reverse }) {
     <>
       <Card reverse={reverse}>
           <h1>About Page</h1>
-          <p>Nothing is actually loading. This is just to display the loading animation which, unfortunately, is barely visible because loading happens very quickly for this app.</p>
+          <p>Nothing is actually loading. This is just to display the loading animation which, unfortunately, is not visible because loading happens very quickly for this app.</p>
           <Link to="/">Back to Home page</Link>
       </Card>
       <Spinner/>
